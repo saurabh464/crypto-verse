1b13d426d649982c97824c0dfffe6efcbcbe8c91
Saurabh <saurabhkumar46485@gmail.com>
2023-08-23 11:47:03 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from './App';
+import { Provider } from "react-redux";
+// import 'antd/dist/antd.css';
+import store from "./app/store";
+import 'antd/dist/reset.css';
+
+ReactDOM.render(
+        <Router>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </Router>
+        , document.getElementById('root')
+);
